a1b2a90262f379db07c29c4509671d84ae032e27
Everythingbeginsfromasinglespeckofdust <154667868+Everythingbeginsfromasinglespeckofdust@users.noreply.github.com>
2024-05-14 09:45:33 +0900

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,25 @@
+function goToPage(pageName) {
+    window.location.href = pageName;
+}
+
+function goToProblem(problemPage) {
+    window.location.href = problemPage;
+}
+
+function setCookie(name, value, days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = 'expires=' + date.toUTCString();
+    document.cookie = name + '=' + value + ';' + expires + ';path=/';
+}
+
+function getCookie(name) {
+    const nameEQ = name + '=';
+    const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
